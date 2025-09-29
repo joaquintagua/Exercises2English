1bd564574699fe2b402f35447c705e768c295ba4
joaquin.tagua <joaquin.tagua@106-28.info2>
2025-09-29 09:42:25 +0200

    Ejercicios 2 Inglés

---
+++ EjerciciosParte2/src/ejercicios2/Ejercicios2.java
@@ -0,0 +1,49 @@
+package ejercicios2;
+
+import java.util.Scanner;
+
+public class Ejercicios2 {
+
+	public static void main(String[] args) {
+	
+	/*
+	 BEGIN
+	 	DISPLAY program title
+	 	DISPLAY prompt for price
+	 	ENTER price
+	 	DYSPLAY prompt for tax
+	 	ENTER tax
+	 	SET price TO price * (1 + tax/100)
+	 	DYSPLAY new price
+	 	END
+	 */
+	
+	//Variables
+	double price;
+	double tax;
+	double newPrice;
+		
+	Scanner sc = new Scanner (System.in);
+	//DYSPLAY program title
+	System.out.println("Program Title");
+	//DYSPLAY prompt for price
+	System.out.println("price");
+	//ENTER price
+	System.out.println("Enter price");
+	price = sc.nextDouble();
+	//DYSPLAY prompt for tax
+	System.out.println("tax");
+	//ENTER tax
+	System.out.println("Enter tax");
+	tax = sc.nextDouble();
+	//SET price TO price * (1 + tax/100)
+	newPrice = (price + (price + tax)/100);
+	//DYSPLAY new price
+	System.out.println("new price");
+	System.out.println(newPrice);
+	
+	sc.close();
+		
+	}
+
+}
